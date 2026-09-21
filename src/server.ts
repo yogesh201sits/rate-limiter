0f524a86934f0e19352a5ced93f4ebbf522aa177
yogesh201sits <yogeshjamdade201@gmail.com>
2026-09-21 21:41:49 +0530

    feta created bun server

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+
+const port = Number(process.env.PORT ?? 3000);
+
+console.log(`Rate limiter running on http://localhost:${port}`);
+
+export default {
+  port,
+  fetch: app.fetch,
+};
